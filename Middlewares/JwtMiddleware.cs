b2998e9bd6280e81ee00584c971ae63e51fb802a
stgustavo <87985856+stgustavo@users.noreply.github.com>
2022-02-01 13:43:05 -0300

    Update JwtMiddleware.cs
    Remove comments

--- Middlewares/JwtMiddleware.cs
+++ Middlewares/JwtMiddleware.cs
@@ -69,34 +69,10 @@ namespace Petaframework.Middlewares
                     Token = token
                 }).Result;
 
-                //var userId = response.Claims.First(x => x.Type == _appSettings.UserLoginClaim).Value;
-
-                // attach user to context on successful jwt validation
-                //context.Items["User"] = userManagerService.GetUserById(userId);
-
                 var claimsIdentity = new ClaimsIdentity(response.Claims, CookieAuthenticationDefaults.AuthenticationScheme);
                 var authProperties = new AuthenticationProperties
                 {
-                    //AllowRefresh = <bool>,
-                    // Refreshing the authentication session should be allowed.
-
-                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    // The time at which the authentication ticket expires. A 
-                    // value set here overrides the ExpireTimeSpan option of 
-                    // CookieAuthenticationOptions set with AddCookie.
-
-                    //IsPersistent = true,
-                    // Whether the authentication session is persisted across 
-                    // multiple requests. When used with cookies, controls
-                    // whether the cookie's lifetime is absolute (matching the
-                    // lifetime of the authentication ticket) or session-based.
-
-                    //IssuedUtc = <DateTimeOffset>,
-                    // The time at which the authentication ticket was issued.
-
-                    //RedirectUri = <string>
-                    // The full path or absolute URI to be used as an http 
-                    // redirect response value.
+   
                 };
                 var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
                 context.User = claimsPrincipal;
@@ -158,12 +134,6 @@ namespace Petaframework.Middlewares
                 .Claims
                 .Any();
         }
-
-
-
-
-
-
 
         public class OIdCSettings
         {
